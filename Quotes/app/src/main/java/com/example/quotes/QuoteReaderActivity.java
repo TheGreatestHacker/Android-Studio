2d079c2ece68c2b1fcf274ecb9e5d45870c07018
Bryan Cruz <bcruzcastillo@hawk.iit.edu>
2020-03-11 19:03:30 -0500

    Add android studio starter code for lab3

---
+++ Quotes/app/src/main/java/com/example/quotes/QuoteReaderActivity.java
@@ -0,0 +1,14 @@
+package com.example.quotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuoteReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote_reader);
+    }
+}
