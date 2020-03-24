d10d65c90d74527aa39eca0436108c2631f6b30f
Bryan Cruz <bcruzcastillo@hawk.iit.edu>
2020-03-23 20:35:16 -0500

    Add small comments and replace the pools default image/strings reference with the new custom ones

--- Quotes/app/src/main/java/com/example/quotes/DataSource.java
+++ Quotes/app/src/main/java/com/example/quotes/DataSource.java
@@ -2,10 +2,12 @@ package com.example.quotes;
 import java.util.ArrayList;
 
 public class DataSource {
+    //data fields
     private ArrayList<Integer> mPhotoPool;
     private ArrayList<Integer> mQuotePool;
     private ArrayList<Integer> mPhotoHdPool;
 
+    //class DataSource default constructor
     public DataSource() {
         mPhotoPool = new ArrayList();
         mQuotePool = new ArrayList();
@@ -16,6 +18,7 @@ public class DataSource {
         setupPhotoHDPool();
     }
 
+    //class methods below
     public ArrayList<Integer> getmPhotoHdPool() {
         return mPhotoHdPool;
     }
@@ -37,14 +40,15 @@ public class DataSource {
         mPhotoPool.add(R.drawable.steve_2);
         mPhotoPool.add(R.drawable.steve_3);
         mPhotoPool.add(R.drawable.steve_4);
-        mPhotoPool.add(R.drawable.steve_5);
-        mPhotoPool.add(R.drawable.steve_6);
-        mPhotoPool.add(R.drawable.steve_7);
-        mPhotoPool.add(R.drawable.steve_8);
-        mPhotoPool.add(R.drawable.steve_9);
-        mPhotoPool.add(R.drawable.steve_10);
+        mPhotoPool.add(R.drawable.apple);
+        mPhotoPool.add(R.drawable.image6);
+        mPhotoPool.add(R.drawable.image7);
+        mPhotoPool.add(R.drawable.image8);
+        mPhotoPool.add(R.drawable.image9);
+        mPhotoPool.add(R.drawable.image10);
     }
     private void setupQuotePool() {
+
         mQuotePool.add(R.string.quote_1);
         mQuotePool.add(R.string.quote_2);
         mQuotePool.add(R.string.quote_3);
@@ -55,19 +59,33 @@ public class DataSource {
         mQuotePool.add(R.string.quote_8);
         mQuotePool.add(R.string.quote_9);
         mQuotePool.add(R.string.quote_10);
+
+        /*
+        mQuotePool.add(R.string.quote_1);
+        mQuotePool.add(R.string.quote_2);
+        mQuotePool.add(R.string.quote_3);
+        mQuotePool.add(R.string.quote_4);
+        mQuotePool.add(R.string.quote_5);
+        mQuotePool.add(R.string.quote_6);
+        mQuotePool.add(R.string.quote_7);
+        mQuotePool.add(R.string.quote_8);
+        mQuotePool.add(R.string.quote_9);
+        mQuotePool.add(R.string.quote_10);
+        */
     }
+
 
     private void setupPhotoHDPool() {
         mPhotoHdPool.add(R.drawable.steve_hd_1);
         mPhotoHdPool.add(R.drawable.steve_hd_2);
         mPhotoHdPool.add(R.drawable.steve_hd_3);
         mPhotoHdPool.add(R.drawable.steve_hd_4);
-        mPhotoHdPool.add(R.drawable.steve_hd_5);
-        mPhotoHdPool.add(R.drawable.steve_hd_6);
-        mPhotoHdPool.add(R.drawable.steve_hd_7);
-        mPhotoHdPool.add(R.drawable.steve_hd_8);
-        mPhotoHdPool.add(R.drawable.steve_hd_9);
-        mPhotoHdPool.add(R.drawable.apple_hd);
+        mPhotoHdPool.add(R.drawable.apple1_hd);
+        mPhotoHdPool.add(R.drawable.image6_hd);
+        mPhotoHdPool.add(R.drawable.image7_hd);
+        mPhotoHdPool.add(R.drawable.image8_hd);
+        mPhotoHdPool.add(R.drawable.image9_hd);
+        mPhotoHdPool.add(R.drawable.image10_hd);
     }
 
 }
